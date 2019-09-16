239b5a3aea5ff887c9d8455d9be23f11e16433f5
zhtluffy <1502075692@qq.com>
2019-09-16 22:48:04 +0800

    enhancement #1 初始化springbootimprovee.

---
+++ src/main/java/com/itszt/springbootimprove/SpringbootimproveApplication.java
@@ -0,0 +1,13 @@
+package com.itszt.springbootimprove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootimproveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootimproveApplication.class, args);
+    }
+
+}
